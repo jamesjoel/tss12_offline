fcd43015c6d5b415b1119aed63da49a36b6ac498
jamesjoel <james.joelj@gmail.com>
2021-09-13 12:32:31 +0530

    node js day 1

---
+++ nodejs/array.js
@@ -0,0 +1,10 @@
+/*
+----------------- Array in JS -------------
+
+when we want to store multi values/data in a single var, then 
+we can use Array.
+
+
+*/
+var x = [100, 200, 500, 1000, 1250];
+console.log(x);
---
+++ nodejs/intro.js
@@ -0,0 +1,52 @@
+// console.log("hello world");
+/*
+-------------------------- Variables in JavaScript ---------------
+The Variables are storing or holding the information/data/value.
+
+Roules for nameing var.
+1. special char not allowed, _ expected.   (var a$b, var a-b, var a b)
+2. start from num not allowed. (var 7a, var a2)
+3. name is a case-sec. (var A)
+
+
+------------------------ DataTypes of Variables ----------------
+
+    1. intiger  (all number +/-)
+
+    2. float    (all decimal number +/-)
+
+    3. boolean  (true/false)
+
+    4. string   (any char, char+num, char+num+spe)
+
+    5. array
+
+    6. object
+
+    7. JSON
+
+    8. undefined    (var not defined)
+
+    9. null         (var a = null; var a;)
+
+    10. empty       (var a = "";)
+
+
+
+
+
+
+*/
+var z;
+
+var a = 100;
+var b = 200.64;
+
+var x = true;
+
+var name = "garvit";
+
+var c = a+b;
+var z = c;
+
+console.log(c);
