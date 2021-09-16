d6777e8d3cd426d3b2fae51db6315332198d5f80
jamesjoel <james.joelj@gmail.com>
2021-09-16 12:29:28 +0530

    loop 1

---
+++ nodejs/conditional3.js
@@ -0,0 +1,23 @@
+var a = "X";
+
+switch(a){
+
+    case "A" : console.log("indore");
+            break;
+
+    case "B" : console.log("bhopal");
+            break;
+
+    case "C" : console.log("mumbai");
+            break;
+
+    case "D" : console.log("pune");
+            break;
+
+    case "E" : console.log("delhi");
+            break;
+
+    default : console.log("no city found");
+}
+
+
---
+++ nodejs/loop1.js
@@ -0,0 +1,36 @@
+/*
+-------------- Loop in Js ----------------
+    1. for()
+
+    2. while()
+
+    3. do.....while()
+
+    4. forEach()
+
+*/
+
+// for(var a = 1; a <= 10; a++)
+// {
+//     console.log(a*5);
+// }
+
+// for(var a = 1; a <= 50; a++)
+// {
+//     var x = a*2;
+//     var y = x-1;
+//     console.log(y);
+// }
+var total = 0;
+for(var a=1; a<=10; a++)
+{
+    // total += a;
+
+}
+
+
+
+
+
+var a = 5; 
+var b = 
