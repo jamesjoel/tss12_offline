2d0dbb623d4f4baf23cf38a207c0444f9230d7d5
garvitmodi885 <garvitjain8855@gmail.com>
2021-09-18 13:53:39 +0530

    garvit update

---
+++ garvit/log.js
@@ -0,0 +1,32 @@
+var a = a
+
+switch(a){
+    case a : console.log("indore");
+              break;
+    case b : console.log("mumbai");
+              break;
+    case c : console.log("bhopal");
+              break;
+    case d : console.log("jobat");
+
+            break;
+
+}    
+
+
+var b = b ;
+
+
+switch(b){
+    case a : console.log("garvit");
+             break;
+    case b : console.log("veshnvi");
+             break;
+    case c : console.log("ujjwal");
+             break;
+    case d : console.log("ankit");
+             break;
+    default : console.log("no city found");                   
+              break;
+
+}
