f71153ba5765dabe61a70637472f4107303b9d95
vaishnavi2596 <89468860+vaishnavi2596@users.noreply.github.com>
2021-09-22 15:48:20 +0530

    return type

---
+++ vaishnavi/pro3/app.js
@@ -0,0 +1,3 @@
+var x = require("./mod");
+var y = x(500000,8,60);
+console.log(y);
---
+++ vaishnavi/pro3/mod.js
@@ -0,0 +1,9 @@
+var emi = (p,r,n)=>{
+    var b = r/100/12;
+    var c = Math.pow((1+b),n);
+    var d = c-1;
+    var e = p*b*c;
+    var ans = e/d;
+    return ans;
+}
+module.exports = emi;
