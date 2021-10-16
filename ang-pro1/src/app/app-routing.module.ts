4b0e8786086140ae686bd98b5e021f1614cb391a
jamesjoel <james.joelj@gmail.com>
2021-10-16 12:31:57 +0530

    component in angular

--- ang-pro1/src/app/app-routing.module.ts
+++ ang-pro1/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
+
+
+const routes: Routes = [
+  {
+    path : "about",
+    component : AboutComponent
+  },
+  {
+    path : "",
+    component :HomeComponent
+  },
+  {
+    path : "contact",
+    component : ContactComponent
+  },
+  {
+    path : "help",
+    component : HelpComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ang-pro1/src/app/app.module.ts
+++ ang-pro1/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Box1Component } from './box1/box1.component';
+import { Box2Component } from './box2/box2.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Box1Component,
+    Box2Component,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
