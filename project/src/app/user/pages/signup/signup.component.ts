26c06c23aadd1d81d017b932a50d265819a6f322
garvitmodi885 <garvitjain8855@gmail.com>
2021-11-17 16:33:15 +0530

    update

--- project/src/app/user/pages/signup/signup.component.ts
+++ project/src/app/user/pages/signup/signup.component.ts
@@ -15,7 +15,6 @@ export class SignupComponent implements OnInit {
 
   checkForm:boolean=false;
 
-<<<<<<< HEAD
         constructor(private _fb:FormBuilder) {
           this.signupForm = this._fb.group({
             full_name: ["", Validators.required],
@@ -27,28 +26,10 @@ export class SignupComponent implements OnInit {
             contact: ["", Validators.required]
           });
         }
-=======
-  constructor(
-    private _fb:FormBuilder,
-    private _user : UserService,
-    private _router : Router
-    ) {
-    this.signupForm = this._fb.group({
-      full_name: ["", Validators.required],
-      email : ["", [Validators.required, Validators.email]],
-      password: ["", Validators.required],
-      re_password: ["", Validators.required],
-      address: ["", Validators.required],
-      city: ["", Validators.required],
-      contact: ["", Validators.required]
-    });
-   }
->>>>>>> c84471b57fc16b9bd27d7b74d6af40084b888a71
 
         ngOnInit(): void {
         }
 
-<<<<<<< HEAD
           signup(){
             if(this.signupForm.invalid){
               this.checkForm=true;
@@ -58,19 +39,3 @@ export class SignupComponent implements OnInit {
         }
         
       } 
-=======
-  signup(){
-    if(this.signupForm.invalid){
-      this.checkForm=true;
-      return;
-    }
-    // console.log(this.signupForm.value);
-    this._user.signup(this.signupForm.value).subscribe((result)=>{
-      // console.log(result);
-      this._router.navigate(["/login"]);
-    })
-    
-  }
-
-}
->>>>>>> c84471b57fc16b9bd27d7b74d6af40084b888a71
