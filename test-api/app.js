d55cc92bef2d2d21f761281fb5c35b5915d3573e
jamesjoel <james.joelj@gmail.com>
2021-11-20 14:15:41 +0530

    api

---
+++ test-api/app.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var sha1 = require("sha1");
+var jwt = require("jsonwebtoken");
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+var userData = [
+    {
+        username : "rohit@gmail.com",
+        password : sha1(123)
+    },
+    {
+        username : "garvit@gmail.com",
+        password : sha1(123)
+    },
+    {
+        username : "ram@gmail.com",
+        password : sha1(123)
+    },
+    {
+        username : "vinita@gmail.com",
+        password : sha1(123)
+    },
+    {
+        username : "vaishnavi@gmail.com",
+        password : sha1(123)
+    }
+    
+]
+
+app.post("/api/user/auth", (req, res)=>{
+    var u = req.body.username;
+    var p = req.body.password;
+    var check = false;
+    for(var i=0; i < userData.length; i++)
+    {
+        if(userData[i] == u)
+        {
+            check = true;
+            break;
+        }
+        
+    }
+
+    if(check == false)
+    {
+        res.status(401).send({ success : false, type : 1});
+    }
+    else{
+        if (userData[i].password == sha1(p))
+        {
+            var token = jwt.sign(userData[i], "hello world");
+            res.status(200).send({ suceses : true , token : token });
+        }
+        else{
+            res.status(401).send({ success : false, type : 2});
+
+        }
+    }
+
+
+
+})
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server running");
+})
+
+
+/*
+    localhost:3000/api/user/auth
+
+    1. make sure your form input types name is 
+            username and password
+
+
+*/
